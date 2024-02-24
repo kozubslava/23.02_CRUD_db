4352dd0f92d931ff2ab7d0886e5cc31903e5487d
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2024-02-24 19:39:35 +0200

    add advanced level

--- sql/dql/select.sql
+++ sql/dql/select.sql
@@ -25,4 +25,28 @@ WHERE full_name = 'Петро';
 
 
 -- *Вивести всіх співробітників віком 27 років або с зарплатнею 1000$
+SELECT *
+FROM workers
+WHERE
+EXTRACT(years from AGE(birthday)) = 23 OR salary = 600;
+
+
+-- *Вивести всіх співробітників віком від 25 (не включно) до 28 років (включно).
+SELECT *
+FROM workers
+WHERE
+EXTRACT(years from AGE(birthday)) > 22 
+AND 
+EXTRACT(years from AGE(birthday)) <= 34;
+
+-- *Вивести всіх співробітників віком від 23 до 27 років або з зарплатнею від 400$ до 1000$.
+SELECT *
+FROM workers
+WHERE EXTRACT(years from AGE(birthday)) > 22 AND EXTRACT(years from AGE(birthday)) <= 30
+OR 
+salary > 400 AND salary < 1000;
+
+
+
+
 
